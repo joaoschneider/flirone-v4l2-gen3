a041b6a9afa45368d248cf4bc204ff5c5000a2a3
Joao <jp1007.dev@gmail.com>
2024-08-27 17:15:17 -0300

    fix planck values

--- src/plank.h
+++ src/plank.h
@@ -1,11 +1,13 @@
 // -- define Flir calibration values ---------------
 // exiftool -plan* FLIROne-2015-11-30-17-26-48+0100.jpg
 
-#define PlanckR1 14331.0
-#define PlanckB 1384.7
+#define PlanckR1 17759.449
+#define PlanckB 1435
 #define PlanckF 1.0
-#define PlanckO 3776
-#define PlanckR2 0.00933092
+#define PlanckO -2604
+#define PlanckR2 0.0125
 
 #define TempReflected 20.0 // Reflected Apparent Temperature [°C]
 #define Emissivity 0.95    // Emissivity of object
+
+17759, 449 0, 0125 1435 1 - 2604
